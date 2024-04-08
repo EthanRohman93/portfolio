ca5ed00a19c0e9e5ec6bab17c0787a461b703619
EthanRohman93 <ethanmrohman@gmail.com>
2024-04-07 21:52:28 -0500

    file names secured without api call

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -1,31 +1,9 @@
-"use client";
-import { useEffect, useState } from 'react';
-import Link from 'next/link';
-import { Project } from '../types/Project';
-import { fetchProjectsAPI } from '../lib/projectsService';
+import ProjectDisplay from "../components/ProjectDisplay"
 
-const ProjectsPage: React.FC = () => {
-  const [projects, setProjects] = useState<Project[]>([]);
-
-  useEffect(() => {
-    fetchProjectsAPI().then(setProjects).catch(console.error);
-  }, []);
-
-  return (
-    <div>
-      <h1>Projects</h1>
-      <ul>
-        {projects.map((project) => (
-          <li key={project.pname}>
-            <Link href={`/project/${project.pname}`}>
-              {project.pname}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
-
-export default ProjectsPage;
-
+export default function Project() {
+    return ( 
+        <main>
+            <ProjectDisplay/>
+        </main>
+    )
+}
